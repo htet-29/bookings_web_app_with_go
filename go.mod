d7a5ddb178e21864c9b8e94294331508f37ceed1
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-11-03 00:31:38 +0630

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/htet-29/bookings
+
+go 1.25.3
+
+require (
+	github.com/alexedwards/scs/v2 v2.9.0
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/justinas/nosurf v1.2.0
+)
---
+++ pkg/handlers/handler.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/htet-29/bookings/pkg/config"
+	"github.com/htet-29/bookings/pkg/models"
+	"github.com/htet-29/bookings/pkg/render"
+)
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.gohtml", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.gohtml", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
